00b807f3a6438d3646325ed25a2ceade72926359
Jackson Bates <jacksonbates@hotmail.com>
2019-11-26 09:45:35 +1100

    webpack shenanigans

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const WorkboxPlugin = require("workbox-webpack-plugin");
+
 module.exports = {
   module: {
     rules: [
@@ -27,6 +29,12 @@ module.exports = {
     new HtmlWebPackPlugin({
       template: "./src/index.html",
       filename: "./index.html",
+    }),
+    new WorkboxPlugin.GenerateSW({
+      // these options encourage the ServiceWorkers to get in there fast
+      // and not allow any straggling "old" SWs to hang around
+      clientsClaim: true,
+      skipWaiting: true,
     }),
   ],
 };
